568f94f0c84b28c743bf8b6de44e9b4e03644d8a
Dipankar Bhaduri <dipankarbhaduri8@gmail.com>
2023-11-28 08:48:06 +0530

    added code for record

---
+++ src/main/java/coreJava/records/EmployeeRecord.java
@@ -0,0 +1,19 @@
+package coreJava.records;
+
+public record EmployeeRecord(String name, int age) {
+    public EmployeeRecord {
+        if (age < 0) {
+            throw new RuntimeException("Age can not be negative");
+        }
+    }
+
+    public void printDetails () {
+        System.out.println("Name : "+name+", Age : "+age);
+    }
+
+    public static void heyPublicStatic () {
+        System.out.println("I AM PUBLIC STATIC INSIDE RECORD");
+    }
+
+    static int num;
+}
--- src/main/java/coreJava/records/RecordTutorials.java
+++ src/main/java/coreJava/records/RecordTutorials.java
@@ -2,6 +2,20 @@ package coreJava.records;
 
 public class RecordTutorials {
     public static void main(String[] args) {
+        EmployeeRecord employeeRecord = new EmployeeRecord("Dipankar", 26);
+        System.out.println(employeeRecord.name());
+        System.out.println(employeeRecord.age());
 
+        EmployeeRecord.num = 1100101100;
+        System.out.println(EmployeeRecord.num);
+        employeeRecord.printDetails();
+        EmployeeRecord.heyPublicStatic();
     }
 }
+
+/*
+  1. Record can not extend by other class
+  2. Inside record, we can declare any variable, but we have to assign some value
+  3. Without declared value need to add in method parameters
+  4. We can implement any interface
+ */
