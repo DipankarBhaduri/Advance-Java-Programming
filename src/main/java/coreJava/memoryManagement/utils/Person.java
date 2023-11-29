6365e053e0104a91d046c2b8a1faf04b9e821d88
Dipankar Bhaduri <dipankarbhaduri8@gmail.com>
2023-11-29 06:44:00 +0530

    added code for memory management

--- src/main/java/coreJava/memoryManagement/utils/Person.java
+++ src/main/java/coreJava/memoryManagement/utils/Person.java
@@ -5,6 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,4 +16,14 @@ import lombok.Setter;
 public class Person {
     private String name;
     private int age;
+    List<String> stringList;
+
+    public void addValueInList() {
+        stringList = new ArrayList<>(Arrays.asList("DIPU", "RAJU", "PUJA"));
+        System.out.println(stringList.toString());
+    }
+
+    public void addValueInList2() {
+        System.out.println(stringList.toString());
+    }
 }
