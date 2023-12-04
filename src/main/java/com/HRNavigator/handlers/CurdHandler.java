e1795a16ade9383e965a320520fc992436aa4638
Dipankar Bhaduri <dipankarbhaduri8@gmail.com>
2023-12-04 08:47:44 +0530

    added code for hr navigator

---
+++ src/main/java/com/HRNavigator/handlers/CurdHandler.java
@@ -0,0 +1,7 @@
+package com.HRNavigator.handlers;
+
+import java.util.Map;
+
+public interface CurdHandler {
+    public void execute (Class<?> clazz, Object object, Map<String, Object> result);
+}
---
+++ src/main/java/com/HRNavigator/handlers/saveHandler/StudentSaveHandler.java
@@ -0,0 +1,13 @@
+package com.HRNavigator.handlers.saveHandler;
+
+import com.HRNavigator.handlers.CurdHandler;
+import org.springframework.stereotype.Component;
+import java.util.Map;
+
+@Component
+public class StudentSaveHandler implements CurdHandler {
+    @Override
+    public void execute(Class<?> clazz, Object object, Map<String, Object> result) {
+
+    }
+}
